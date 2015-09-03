0150ff9579435bc0d82983ca884ac5dce9a66723
Ryan Clarke <ryan@ryanclarke.net>
2015-09-03 08:05:15 -0400

    Always open vim in background with 'v'

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -3,13 +3,17 @@
 . ~/dotfiles/bash/os_vars.sh
 . ~/dotfiles/bash/git_helpers.sh
 
+function v() {
+  __rsc__vim $1 &
+}
+
 alias ..='cd ..'
 alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
 alias ......='cd ../../../../../'
-alias ba='__rsc__vim ~/dotfiles/bash/bash_profile.sh &'
-alias bl='__rsc__vim ~/.bash_profile &'
+alias ba='v ~/dotfiles/bash/bash_profile.sh'
+alias bl='v ~/.bash_profile'
 alias c-='cd -'
 alias cdd='cd /c/dev/'
 alias df='cd ~/dotfiles'
@@ -18,9 +22,8 @@ alias h='cd ~'
 alias la='__rsc__ls_color -F'
 alias ll='__rsc__ls_color -AFhl'
 alias q='exit'
-alias v='__rsc__vim'
-alias va='__rsc__vim ~/dotfiles/vim/vimrc.vim &'
-alias vl='__rsc__vim __rsc__vimrc'
+alias va='v ~/dotfiles/vim/vimrc.vim'
+alias vl='v __rsc__vimrc'
 alias x='__rsc__filebrowser '
 
 function cl() {
--- bash/os_vars.sh
+++ bash/os_vars.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 function __rsc__windows_vars() {
-  __rsc__vimrc='~/_vimrc'
   alias __rsc__filebrowser='explorer '
   alias __rsc__ls_color='ls --color '
   alias __rsc__vim='gvim '
-  alias __rsc__vimrc='$__rsc__vimrc '
+  alias __rsc__vimrc='~/_vimrc '
+  alias __rsc__vim_dir='~/vimfiles/ '
 }
 
 function __rsc__linux_vars() {
-  __rsc__vimrc='~/.vimrc'
   alias __rsc__filebrowser='nautilus '
   alias __rsc__ls_color='ls --color '
   alias __rsc__vim='gvim '
-  alias __rsc__vimrc='$__rsc__vimrc '
+  alias __rsc__vimrc='~/.vimrc '
+  alias __rsc__vim_dir='~/.vim/ '
 }
 
 function __rsc__osx_vars() {
-  __rsc__vimrc='~/.vimrc'
   alias __rsc__filebrowser='open '
   alias __rsc__ls_color='ls -G '
-  alias __rsc__vim='macvim '
-  alias __rsc__vimrc='$__rsc__vimrc '
+  alias __rsc__vim='mvim '
+  alias __rsc__vimrc='~/.vimrc '
+  alias __rsc__vim_dir='~/.vim/ '
 }
 
 function __rsc__set_os() {
