e05ab81244d2040d41fa871b8addc4afa7d25f5a
Ryan Clarke <ryan@ryanclarke.net>
2015-10-22 08:53:50 -0400

    Tweak H2W PS1

--- bash/ps1_prompt_h2w.sh
+++ bash/ps1_prompt_h2w.sh
@@ -1,4 +1,4 @@
-__rsc__rulechars="--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+__rsc__rulechars="········································································································································································································"
 
 function prompt_command {
   reset="\[\033[0m\]"
@@ -6,6 +6,7 @@ function prompt_command {
   gray="\[\033[1;30m\]"
   yellow="\[\033[33m\]"
   cyan="\[\033[36m\]"
+  blue="\[\033[34m\]"
   bluebackground="\[\033[44m\]"
 
   if [ -z "$h2wtimestamp" ] ; then 
@@ -13,15 +14,15 @@ function prompt_command {
   fi
   local timeouttime="`date -d '10 minutes ago' +%s`"
   if [ $h2wtimestamp -lt $timeouttime ] ; then #if it's less than, it's older
-    H2WSTATUS="[ Health2Wealth - `h2w quickstats` ]"
+    H2WSTATUS="[ H2W - `h2w quickstats` ]"
     H2WSTATUSLENGTH="$(echo $H2WSTATUS | wc -c)"
     h2wtimestamp="`date +%s`"
   fi
 
-  let "rulelength = $(tput cols) - $H2WSTATUSLENGTH - 8"
+  let "rulelength = $(tput cols) - $H2WSTATUSLENGTH - 8 - 6"
   horizontal_rule="${__rsc__rulechars:0:$rulelength}"
 
-  H2W="$bluebackground$H2WSTATUS$reset"
+  H2W="$bluebackground$H2WSTATUS$reset$blue▓▒░$reset"
   TIME="$gray\A$reset"
   HRULE="$gray$horizontal_rule$reset"
   COMMANDNUMBER="$red{$gray!\!$red}$reset"
