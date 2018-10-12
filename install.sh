dbaea7f73120f9b6a4c437669432a0c27a53f854
Ryan Clarke <ryan@ryanclarke.net>
2018-10-12 08:14:15 -0400

    Move vim setup code to its own script

--- install.sh
+++ install.sh
@@ -7,20 +7,5 @@ function source_sh_scripts() {
   echo "$bash_profile_caller" >> ~/.bash_profile
 }
 
-function source_vimrc() {
-  vimrc_caller="source ~/dotfiles/vim/vimrc.vim"
-  pathogen="execute pathogen#infect()"
-  echo "$pathogen" >> $(eval echo $__rsc__vimrc)
-  echo "$vimrc_caller" >> $(eval echo $__rsc__vimrc)
-}
-
-function install_vim_plugins() {
-  mkdir -p $(eval echo $__rsc__vim_dir/autoload) $(eval echo $__rsc__vim_dir/bundle)
-  curl -LSso $(eval echo $__rsc__vim_dir/autoload/pathogen.vim https://tpo.pe/pathogen.vim)
-  git clone https://github.com/scrooloose/nerdtree.git $(eval echo $__rsc__vim_dir/bundle/nerdtree)
-}
-
 source_sh_scripts
-source_vimrc
-install_vim_plugins
 
